f931d6087b5698642a96d681ee2f5dc7754e6e14
cleitao-anao <joaov.cc2005@gmail.com>
2025-07-11 13:42:12 -0300

    erros concertados

--- codinando/settings.py
+++ codinando/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+import whitenoise
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -39,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'whitenoise.runserver_nostatic',
     'portfolio.apps.PortfolioConfig',
 ]
 
@@ -52,8 +52,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'portfolio.middleware.AdminOnlyMiddleware',
     'portfolio.middleware.project_view.ProjectViewMiddleware',
-    'portfolio.middleware.AdminOnlyMiddleware',  # Middleware personalizado
 ]
 
 ROOT_URLCONF = 'codinando.urls'
@@ -134,6 +134,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Configuração do WhiteNoise
+WHITENOISE_USE_FINDERS = True
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Configurações de mídia
