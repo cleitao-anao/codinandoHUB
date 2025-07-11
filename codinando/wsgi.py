5882bcd91f7a9a228998186eba177e685a3e612a
cleitao-anao <joaov.cc2005@gmail.com>
2025-07-11 13:55:40 -0300

    teste 2

--- codinando/wsgi.py
+++ codinando/wsgi.py
@@ -10,7 +10,12 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
+import os
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'codinando.settings')
 
 application = get_wsgi_application()
+application = WhiteNoise(application, root='staticfiles')
+application.add_files('static', prefix='static/')
+application.add_files('media', prefix='media/')
