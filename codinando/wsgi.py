00c5c6a0dc459a12d388cc00df133fc2fe250742
cleitao-anao <joaov.cc2005@gmail.com>
2025-07-11 14:04:27 -0300

    teste 3

--- codinando/wsgi.py
+++ codinando/wsgi.py
@@ -2,20 +2,22 @@
 WSGI config for codinando project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
-import os
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'codinando.settings')
 
-application = get_wsgi_application()
-application = WhiteNoise(application, root='staticfiles')
-application.add_files('static', prefix='static/')
-application.add_files('media', prefix='media/')
+# Cria a aplicação WSGI do Django
+django_app = get_wsgi_application()
+
+# Configura o WhiteNoise para servir arquivos estáticos
+app = WhiteNoise(django_app, root='staticfiles')
+app.add_files('static', prefix='static/')
+app.add_files('media', prefix='media/')
+
+# O Vercel espera uma variável 'app' ou 'handler'
+application = app
+handler = app  # Para compatibilidade com o Vercel
